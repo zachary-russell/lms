a53ff9c28189df0a9ebb7fa8b55b052af50522f0
Zach Russell <zach@protechig.com>
2017-07-04 01:51:24 -0400

    add a puggerpillar

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 var express = require('express');
 var app = express();
 var db = require('./lib/db/rethinkdb.js');
+var pug = require('pug');
+app.set('view engine', 'pug');
 db.init(function(err, response) {
     if(err) throw err;
 })
 app.get('/', function(req, res) {
-    db.addLink(1, {name: "The_link"}, function(err, response) {
-          if(err) throw err;
-
-    });
     var params = req.query
     db.getLinks(params, function(err, response) {
         if(err) throw err;
         res.send(response);
     })
+});
+app.get('/hello', function(req, res) {
+    res.render('index', { title: 'Hey', message: 'Hello there!' })
 });
 
 app.listen(3000, function() {
